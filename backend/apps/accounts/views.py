2fe9b022c2a15bbbbd82df48a9da116cfec891de
Vitaly Sem <Vir2S@users.noreply.github.com>
2022-10-09 19:29:47 +0200

    Refactor/separate user and role (#3)
    * User and Role separated
    
    * User and Role separated

--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -1,13 +1,27 @@
 from rest_framework.viewsets import ModelViewSet
 
 
-from .models import User
-from .serializers import UserSerializer
+from .models import User, Role
+from .serializers import UserSerializer, RoleSerializer
 
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = User.objects.filter(id=user.id).all()
+
+        if user.is_staff:
+            queryset = User.objects.all()
+
+        return queryset
+
+
+class RoleViewSet(ModelViewSet):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
 
     def get_queryset(self):
         user = self.request.user
